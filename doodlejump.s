84748470966e0af0e03e31c4bd550107ac324fee
Iris-0829 <1648333063@qq.com>
2020-11-25 14:42:41 -0500

    doodler can go left or right

--- doodlejump.s
+++ doodlejump.s
@@ -45,7 +45,7 @@ bgcolor: .word 0xffffff  # white
 pfcolor: .word 0x00ff00  # green
 ddcolor: .word 0x0000ff  # blue
 ddpos_x: .word 100
-ddpos_y: .word 15
+ddpos_y: .word 10
 	
 .text
 
@@ -53,26 +53,53 @@ main:
 
 draw:
 	# draw background
-	jal drawbg
+	#jal drawbg
+	#drawbg:        
+	lw $t0, displayAddress	# base address for display
+	lw $t1, bgcolor		# red 
+	
+	add $t2, $zero, $zero  # offset
+	add $t3, $zero, 4096  # limit
+bg:	
+	bge $t2, $t3, endbg
+	add $t4, $t2, $t0 
+	sw $t1, 0($t4)  # paint white background
+	addi $t2, $t2, 4
+	j bg
+	
+endbg:
+	#jr $ra
 	
 	# check keyboard press
 	lw $t8, 0xffff0000
  	beq $t8, 1, keyboard_input
  	j end_key
-keyboard_input:
+keyboard_input:  # a key is pressed
 	lw $t2, 0xffff0004
 	beq $t2, 0x6A, respond_to_J
 	beq $t2, 0x6B, respond_to_K
 	j end_key
-respond_to_J:  # player should go left
-	li $v0, 1		  
-	la $a0, 7	
-	syscall	
+respond_to_J:  # j is pressed, player should go left
+	lw $t3, ddpos_x
+	addi $t3, $t3, -4
+	blez $t3, go_to_right
+	j save_x_J
+go_to_right:
+	addi $t3, $t3, 128
+save_x_J:
+	sw $t3, ddpos_x
 	j end_key
-respond_to_K:  # player should go right
-	li $v0, 1		  
-	la $a0, 8	
-	syscall	
+	
+respond_to_K:  # k is pressed, player should go right
+	lw $t3, ddpos_x
+	addi $t3, $t3, 4
+	addi $t4, $t4, 128
+	bge $t3, $t4, go_to_left
+	j save_x_K
+go_to_left:
+	addi $t3, $t3, -128
+save_x_K:
+	sw $t3, ddpos_x
 	j end_key
 
 end_key:
@@ -106,36 +133,19 @@ end_key:
 	# update new loc (drop)
 	lw $t4, ddpos_y
 	addi $t4, $t4, 1
-	sw $t4, ddpos_y
+	#sw $t4, ddpos_y
 	
 	
 	# sleep
-	#li $v0, 32
-	#li $a0, 1
-	#syscall
+	li $v0, 32
+	li $a0, 80
+	syscall
 	
         
         j draw
 
-drawbg:        
-	lw $t0, displayAddress	# base address for display
-	lw $t1, bgcolor		# red 
-	
-	add $t2, $zero, $zero  # offset
-	add $t3, $zero, 4096  # limit
 
-bg:	
-	bge $t2, $t3, endbg
-	add $t4, $t2, $t0 
-	sw $t1, 0($t4)  # paint white background
-	addi $t2, $t2, 4
-	j bg
-	
-endbg:
-	jr $ra
-	
-	
-
+#=========draw the platform================
    
 drawpf: 
 	# load loc from stack
@@ -160,7 +170,7 @@ endpf:
 	jr $ra
 	
 	
-	
+#=========draw the doodler================
 drawdd: 
 	# load loc from stack
 	lw $t2, 0($sp)  # start point
@@ -189,6 +199,4 @@ drawdd:
 Exit:
 	li $v0, 10 # terminate the program gracefully
 	syscall
-
-
 
